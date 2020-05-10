fad7558bff617a4c66357acd545cb219c67302b9
itisDouglas <dougcueva@Evelyns-iMac.local>
2020-05-10 15:23:48 -0400

    added additional views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
+#rule for views:
+#a view needs to return an HttpResponse object or raise exception like Http 404
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #loading up the template
+    template = loader.get_template('polls/index.html')
+    #context is a dictionary mapping template variable names to Python objects
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #render function takes request objects as first argument
+    #template name as second argument
+    #dictionary as optional third argument
+    #returns an HttpResponse object of the given template 
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    #get_object_or_404() takes django model as 1st argument
+    #takes arbitrary number of keyword arguments as secon argument
+    #passes these to the get() of the model's manager
+    #raises http 404 if object doesn't exist
+    question = get_object_or_404(Question,
+    pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
