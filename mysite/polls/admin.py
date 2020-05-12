71b9029bf79cae1a5ca2366f264585ee92293c7a
itisDouglas <dougcueva@Evelyns-iMac.local>
2020-05-11 20:42:20 -0400

    added testing

--- mysite/polls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Question
-
-#telling admin that Question objects have an admin interface
-admin.site.register(Question)
